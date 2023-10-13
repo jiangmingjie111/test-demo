58156f466543774c8e9f09d7b4d0cef5f4526ecb
root <root@ip-172-31-16-110.cn-north-1.compute.internal>
2023-10-13 09:31:24 +0000

    init

---
+++ src/main/java/com/hpe/java/helloworld/Hello.java
@@ -0,0 +1,10 @@
+package com.hpe.java;
+//这是一个问好程序
+public class Hello {
+    //一个类只能有一个main方法
+    public static void main(String arg[]) {
+        System.out.print("helloworld!");//输出不换行
+        System.out.println("heng");//输出换行
+        System.out.print("jadh");
+    }
+}
