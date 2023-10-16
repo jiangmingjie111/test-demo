b006c5c6376972d5d7d3d484ae022c75df083965
root <root@ip-172-31-29-218.cn-north-1.compute.internal>
2023-10-16 02:46:41 +0000

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome to Aalaxy" }');
+    res.send('{ "response": "Hello, Welcome to Aalaxy,Today is Monday" }');
 });
 
 app.get('/will', function (req, res) {
